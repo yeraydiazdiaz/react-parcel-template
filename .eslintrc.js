7d94052c922ecd0c5cf3a551b03dac9f8704f8e3
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2018-06-24 15:10:49 +0100

    ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": ["standard", "plugin:react/recommended"],
+  "plugins": ["react"],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "env": {
+    "mocha": true
+  }
+};
